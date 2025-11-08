95d9994ec9d886eda1d2a955babae6a3a928a85b
LimeIvy <raimu10311221@yahoo.co.jp>
2025-11-08 12:58:32 +0900

    footer-tabの追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
+import * as React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+
+import Compare from "./components/Compare";
+import History from "./components/History";
 import Map from "./components/Map";
+import Settings from "./components/Settings";
+
+const Tab = createBottomTabNavigator();
 
 const App = () => {
-  return <Map />;
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={Map} />
+        <Tab.Screen name="Compare" component={Compare} />
+        <Tab.Screen name="History" component={History} />
+        <Tab.Screen name="Settings" component={Settings} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
 };
 
 export default App;
---
+++ src/components/Compare.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const Compare = () => {
+  return (
+    <View>
+      <Text>Compare</Text>
+    </View>
+  );
+};
+
+export default Compare;
