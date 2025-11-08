9c6887e82f292d7f030cbe356a951ad2c2869fb6
LimeIvy <raimu10311221@yahoo.co.jp>
2025-11-08 13:03:32 +0900

    footer-tabにアイコンを追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import Ionicons from "@expo/vector-icons/Ionicons";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
 
@@ -12,7 +13,25 @@ const Tab = createBottomTabNavigator();
 const App = () => {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName: keyof typeof Ionicons.glyphMap = "help-outline";
+
+            if (route.name === "Home") {
+              iconName = focused ? "home" : "home-outline";
+            } else if (route.name === "Compare") {
+              iconName = focused ? "git-compare" : "git-compare-outline";
+            } else if (route.name === "History") {
+              iconName = focused ? "time" : "time-outline";
+            } else if (route.name === "Settings") {
+              iconName = focused ? "settings" : "settings-outline";
+            }
+
+            return <Ionicons name={iconName} color={color} size={size} />;
+          },
+        })}
+      >
         <Tab.Screen name="Home" component={Map} />
         <Tab.Screen name="Compare" component={Compare} />
         <Tab.Screen name="History" component={History} />
